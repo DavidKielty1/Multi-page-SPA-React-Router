17a038cecc0bed6ee2fa198740ae7ebd9dec1c5a
DavidKielty1 <davidkielty@live.co.uk>
2022-10-10 13:38:21 +0100

    Route, Switch, exact, Redirect, nested routing, useParams for dynamic routing, Link for href to

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
+import ProductDetails from "./pages/ProductDetails";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
